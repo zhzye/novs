cfa4bfd810b63dc4c692ac734a499bf545b23847
zhzye <zhzyer@gmail.com>
2019-02-07 21:45:49 +0800

    servlet dispather

---
+++ novs_web/src/main/java/com/zhzye/novs/controller/DispatcherServlet.java
@@ -0,0 +1,49 @@
+package com.zhzye.novs.controller;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import javax.servlet.*;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+
+public class DispatcherServlet extends GenericServlet {
+    ApplicationContext applicationContext;
+
+    @Override
+    public void init() throws ServletException {
+        super.init();
+        applicationContext = new ClassPathXmlApplicationContext("spring.xml");
+    }
+
+    @Override
+    public void service(ServletRequest servletRequest, ServletResponse servletResponse) throws ServletException, IOException {
+        HttpServletRequest httpServletRequest = (HttpServletRequest) servletRequest;
+        HttpServletResponse httpServletResponse = (HttpServletResponse) servletResponse;
+        String path = httpServletRequest.getServletPath().substring(1);
+        String beanName = null;
+        String methodName = null;
+        int index = path.indexOf('/');
+        if (index != -1) {
+            beanName = path.substring(0, index) + "Controller";
+            methodName = path.substring(index + 1, path.indexOf(".do"));
+        } else {
+            beanName = "selfController";
+            methodName = path.substring(0, path.indexOf(".do"));
+        }
+        Object object = applicationContext.getBean(beanName);
+        try {
+            Method method = object.getClass().getMethod(methodName, HttpServletRequest.class, HttpServletResponse.class);
+            method.invoke(object, httpServletRequest, httpServletResponse);
+        } catch (NoSuchMethodException e) {
+            e.printStackTrace();
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        } catch (InvocationTargetException e) {
+            e.printStackTrace();
+        }
+    }
+}
